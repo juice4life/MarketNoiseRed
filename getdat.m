3c3f59fc0fb32f9b4d413929337c8e9241e0086a
David Buzinski <home@Davids-MacBook-Pro-3.local>
2014-12-18 20:26:58 -0500

    added new comments to make code clearer

--- getdat.m
+++ getdat.m
@@ -1,23 +1,27 @@
-%This is a program to remove data from Google Finance sheet
-%First copy sheet from internet and get rid of top lines
-%Next replace all commas with spaces
+%This is a program to create an array containing price 
+%and (optional) volume data over time intervals from a Google Finance sheet
 
 function [M,Vol]=getdat(period,days,ticker)
 
-%Subfunction that creates text file with stock data
+%Subfunction that creates text file with stock data from Google Finance
 pullfromfinance(period,days,ticker);
 
-%This part of the code extracts 2nd column (contains current price)
+%This part of the code extracts desired columns of the txt file (contains current price)
 fileID = fopen('data.txt','r');
+
+%Create a 6 column array A which contains all data from the txt file
 formatSpec = '%f %f %f %f %f %f';
 sizeA = [6 Inf];
 A = fscanf(fileID,formatSpec,sizeA);
 fclose(fileID);
 
+%Use array splicing to extract only the 2nd column of A (the price) and the 6th (the volume)
 M=A(2,:);
 Vol=A(6,:);
 delete('data.txt');
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function pullfromfinance(period,days,ticker)
 
@@ -25,23 +29,34 @@ function pullfromfinance(period,days,ticker)
 str='http://www.google.com/finance/getprices?i=%d&p=%dd&f=d,o,h,l,c,v&df=cpct&q=%s\n';
 url=sprintf(str,period,days,ticker);
 html = urlread(url);
+
+%replace all commas with spaces and get rid of non numbers
 html = strrep(html,',',' ');
 html = strrep(html,'a','');
 
-%Record data to a file
+%Create new file print data from html to it
 fileID = fopen('data.txt','w');
 fprintf(fileID,'%s',html);
 fclose(fileID);
 
-%This part of the code gets rid of unimportant lines at the beginning
+
+%The next part of the code gets rid of unimportant lines at the beginning of the txt file
 fileID = fopen('data.txt','r+');
+
+%Go to the first line of the text file
 frewind(fileID);
+
+%Delete the first 7 columns of html (replace each string with nothing)
 for k=1:7;
     html=strrep(html,fgets(fileID),'');
 end
 fclose(fileID);
 
+
+%Write reformated html data to txt file
 fileID = fopen('data.txt','w');
 fprintf(fileID,'%s',html);
 fclose(fileID);
 end
+
+
