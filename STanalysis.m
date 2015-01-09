6ac14000ed1c63a29eb13aae3e008c34d92ad619
David Buzinski <home@Davids-MacBook-Pro-3.local>
2015-01-09 11:54:07 -0500

    added to STanalysis.m

--- STanalysis.m
+++ STanalysis.m
@@ -1,6 +1,6 @@
 function data=STanalysis(ticker,dip)
 %This program performs short term analysis on 
-%dips and spikes of a given stock
+%dips and spikes of a given option
 
 period=60;
 days=30;
@@ -13,18 +13,30 @@ price=getdat(period,days,ticker);
 spacings=SpacingData(price);
 %Save spacing data
 
-sigma=var(spacings);
-mu=mean(spacings);
-%Collect spacing statistics
 
-for j=1:length(spacings)-1;
-	if spacings(j+1)-spacings(j)>=dip;
-		while k==0;
-			;
-			;
-			;
+diprange=.01:.01:floor(var(spacings)*100)/100;
+%We measure 
+
+timedata=cell(1,length(diprange));
+
+for dip=diprange;
+	for j=1:length(spacings)-1;
+		if spacings(j+1)-spacings(j)>=dip && ;
+			k=0;
+			ind=j+2;
+			while k==0;
+				if spacings(ind)>= spacings(j);
+					t=ind-j;
+					k=1;
+				elseif ind==length(spacings);
+					k=1;
+				else
+					ind=ind+1;
+				end
+			end
 		end
 	end
+	timedata
 end
 
 end
